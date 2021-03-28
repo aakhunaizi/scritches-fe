fe2a5e40e849317fd647a99255f49b87cd94f4b1
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-03-28 22:46:55 +0300

    Added sign in and sign out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // Components
-import SignUp from "./components/User/SignUp";
 import NavBar from "./components/Navbar";
+import Routes from "./components/Routes";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Scritches</title>
+      </Helmet>
       <NavBar />
-      <SignUp />
+      <Routes />
     </div>
   );
 }
