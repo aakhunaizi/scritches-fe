00ab34a52c7ecfe59367ad5d67e799293eb424dc
HawraAmeer <hwra.ameer97@gmail.com>
2021-04-04 11:09:43 +0300

    Added Loading Component & Not Found Page

---
+++ src/components/Loading/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { CircularProgress } from "@material-ui/core";
+
+export const StyledLoading = styled(CircularProgress)`
+  margin-top: 10%;
+  margin-left: 50%;
+`;
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div style={{ marginTop: "5%" }}>This page does not exist!</div>;
+};
+
+export default NotFound;
