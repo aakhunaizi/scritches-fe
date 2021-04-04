b01f84b893d5873e7bef4f486aff6b33204cb0a1
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-04-04 20:36:05 +0300

    Modified Not Found component

---
+++ src/components/NotFound/styles.js
@@ -0,0 +1,13 @@
+import { Image } from "react-bootstrap";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: auto;
+  margin-top: 5.5em;
+`;
+
+export const StyledImage = styled(Image)`
+  max-width: 40%;
+`;
