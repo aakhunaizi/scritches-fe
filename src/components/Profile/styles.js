a3992061d57317f32fc595fa23a6fb8c5e13f0d8
HawraAmeer <hwra.ameer97@gmail.com>
2021-04-05 18:28:08 +0300

    Cleaned Profile

--- src/components/Profile/styles.js
+++ src/components/Profile/styles.js
@@ -1,10 +1,27 @@
 import styled from "styled-components";
-import { Button, Avatar, Paper, Divider, Typography } from "@material-ui/core";
 import { Image, Modal } from "react-bootstrap";
+import { Button, Avatar, Paper, Divider, Typography } from "@material-ui/core";
 
-export const StyledProfile = styled.div`
-  flex-grow: 1;
-  margin: 5%;
+export const StyledAddButton = styled(Button)`
+  color: ${(props) => props.theme.palette.orange.main};
+  margin: 2%;
+  &:hover {
+    background-color: ${(props) => props.theme.palette.orange.main};
+    color: white;
+  }
+`;
+
+export const StyledAddButtonFloat = styled(Button)`
+  color: ${(props) => props.theme.palette.orange.main};
+  float: right;
+  &:hover {
+    background-color: ${(props) => props.theme.palette.orange.main};
+    color: white;
+  }
+`;
+
+export const StyledAvatar = styled(Avatar)`
+  background-color: ${(props) => props.theme.palette.lightBlue.main};
 `;
 
 export const StyledEditButton = styled(Button)`
@@ -17,29 +34,11 @@ export const StyledEditButton = styled(Button)`
 
 export const StyledEditButtonMargin = styled(Button)`
   color: ${(props) => props.theme.palette.orange.main};
-  &:hover {
-    background-color: ${(props) => props.theme.palette.orange.main};
-    color: white;
-  }
   margin-top: 3%;
-`;
-
-export const StyledAddButton = styled(Button)`
-  margin: 2%;
-  color: ${(props) => props.theme.palette.orange.main};
   &:hover {
     background-color: ${(props) => props.theme.palette.orange.main};
     color: white;
   }
-`;
-
-export const StyledAddButtonFloat = styled(Button)`
-  color: ${(props) => props.theme.palette.orange.main};
-  &:hover {
-    background-color: ${(props) => props.theme.palette.orange.main};
-    color: white;
-  }
-  float: right;
 `;
 
 export const StyledDetailsButton = styled(Button)`
@@ -50,17 +49,15 @@ export const StyledDetailsButton = styled(Button)`
   }
 `;
 
-export const StyledAvatar = styled(Avatar)`
-  background-color: ${(props) => props.theme.palette.lightBlue.main};
+export const StyledDivider = styled(Divider)`
+  margin-top: 2%;
+  margin-bottom: 2%;
 `;
 
-export const StyledProfileImage = styled(Image)`
-  width: 200px;
-  height: 200px;
-  margin-bottom: 5%;
+export const StyledModal = styled(Modal)`
+  margin-top: 10em;
 `;
 
-// color: ${(props) => props.theme.palette.orange.main};
 export const StyledPaper = styled(Paper)`
   text-align: center;
   padding: 3%;
@@ -72,38 +69,36 @@ export const StyledPaperMargin = styled(Paper)`
   margin-bottom: 3%;
 `;
 
+export const StyledProfile = styled.div`
+  flex-grow: 1;
+  margin: 5em 2em;
+`;
+
+export const StyledProfileImage = styled(Image)`
+  width: 200px;
+  height: 200px;
+  margin-bottom: 5%;
+`;
+
 export const StyledSaveButton = styled(Button)`
   color: ${(props) => props.theme.palette.orange.main};
-  &:hover {
-    background-color: ${(props) => props.theme.palette.orange.main};
-    color: white;
-  }
   float: right;
   margin: 2%;
-`;
-
-export const StyledUploadButton = styled(Button)`
-  color: ${(props) => props.theme.palette.orange.main};
   &:hover {
     background-color: ${(props) => props.theme.palette.orange.main};
     color: white;
   }
-  float: right;
-`;
-
-export const StyledModal = styled(Modal)`
-  margin-top: 10em;
-`;
-
-export const StyledDivider = styled(Divider)`
-  margin-top: 2%;
-  margin-bottom: 2%;
-`;
-
-export const ImageInput = styled.input`
-  display: none;
 `;
 
 export const StyledTypography = styled(Typography)`
   word-wrap: break-word;
+`;
+
+export const StyledUploadButton = styled(Button)`
+  color: ${(props) => props.theme.palette.orange.main};
+  float: right;
+  &:hover {
+    background-color: ${(props) => props.theme.palette.orange.main};
+    color: white;
+  }
 `;
