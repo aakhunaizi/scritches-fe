e980b859ec8d04510b46804a947b604cc8e0ca9a
HawraAmeer <hwra.ameer97@gmail.com>
2021-04-05 11:06:03 +0300

    Created theme.js

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const theme = createMuiTheme({
+  palette: {
+    lightBlue: { main: "#87BCDE" },
+    orange: { main: "#EB5E28", light: "#EE7343" },
+    darkGrey: { main: "#292929" },
+  },
+});
